1cb63a9efce7e1de044622c935f17f91b04e18f2
kunsworth3 <k8unsworth@gmail.com>
2016-03-13 01:39:24 -0500

    more FTA

--- pa1/ftaclient.py
+++ pa1/ftaclient.py
@@ -1,50 +1,91 @@
 # FTA Client with interactive command window
-
+from rtp import *
 import sys
 
 # create a reliable connection with the FTA server
+# return socket
 def connect(host, port, rwnd):
-	pass
-
+	try:
+		sock = RTPSocket()
+		port = int(port)
+		sock.connect((host,port))
+		return sock
+	except:
+		print('Error: Unable to connect to server.')
+		sys.exit(1)
+		
 
 # download file1 from server and upload file2 to the server through the same RTP connection
-def get_post(file1, file2):
+# use multithreading or other way to support multiple simultaneous RTP connections
+def get_post(file1, file2, sock, host, port):
 	pass
 
 
 # download file from server
-def get(file):
-	pass
+def get(file, sock, host, port):
+	# send filename to server
+	sock.send(file, (host, port))
+
+	# todo try 3 times to receive file
+	# receive response from server
+	data, addr = sock.recv()
+	if data == "File not found.":
+		print "Error: File not found."
+	else:
+		print data
+		# wtf do i do with the file now
 
 
 def main(argv):
-
 	# arguments should be H:P W
 	if len(argv) != 2:
-		print('Wrong number of arguments.\npython ftaclient.py $HOST:$PORT $RWND')
-        sys.exit(1)
+		print "Wrong number of arguments.\npython ftaclient.py $HOST:$PORT $RWND"
+		sys.exit(1)
 
-    arg0 = argv[0].split(":")
-    host = arg0[0]
-    port = arg0[1]
-    rwnd = argv[1]
+	arg0 = argv[0].split(":")
+	host = arg0[0]
+	port = int(arg0[1])
+	rwnd = int(argv[1])
 
 	disconnect = False
-	connect()
+	sock = connect(host, port, rwnd)
 
 	while disconnect == False:
 		command = raw_input("> ")
-		if command == disconnect:
+		if command == "disconnect":
 			disconnect = True
+			# disconnect from the server gracefully
+			# todo send FIN to server
+			sock.close()
+
 		elif "get-post" in command:
 			# arguments should be F G
 			# F: download file F from the server
 			# G: upload file G to the server through same RTP connection
-			get_post(f,g)
+			cmd_list = command.split(" ")
+			if len(cmd_list) != 3:
+				print "Wrong number of arguments for get-post (expected 2)"
+				continue
+			f = cmd_list[1]
+			g = cmd_list[2]
+			try:
+				get_post(f,g, sock, host, port)
+			except:
+				print "Error downloading or uploading file."
+
 		elif "get" in command:
 			# argument should be F
 			# F: download file F from the server
-			get(f)
+			cmd_list = command.split(" ")
+			if len(cmd_list) != 2:
+				print "Wrong number of arguments for get (expected 1)"
+				continue
+			f = cmd_list[1]
+			try:
+				get(f, sock, host, port)
+			except:
+				print "Error downloading file."
+				raise # for debugging
 		else:
 			print "Invalid command."
 
--- pa1/ftaserver.py
+++ pa1/ftaserver.py
@@ -1,16 +1,62 @@
 # FTA Server
-
+from rtp import *
 import sys
+import time
+
+# global dictionary to store files. key: filename, value: file (what data type? byte string? help)
+files = {}
+
+# download file1 from client and upload file2 to client in same RTP connection
+def get_post(file1, file2, sock, host, port):
+	pass
+
+
+# upload file to client
+def post(file, sock, host, port):
+	# get file from dictionary
+	send_file = files[file]
+	try:
+		# send file to client
+		sock.send(send_file, (host, port))
+	except:
+		print "Error: Unable to send file."
+
 
 def main(argv):
 
 	# arguments should be port, rwnd
 	if len(argv) != 2:
-		print('Wrong number of arguments.\npython ftaclient.py $PORT $RWND')
-        sys.exit(1)
+		print "Wrong number of arguments.\npython ftaclient.py $PORT $RWND"
+		sys.exit(1)
 
-	port = argv[0]
+	port = int(argv[0])
 	rwnd = argv[1]
+	host = '127.0.0.1'
+
+	# create socket and bind to port
+	try:
+		sock = RTPSocket()
+		sock.bind((host, port))
+		sock.accept()
+
+		# wait for response from client
+		while 1:
+			data,addr = sock.recv()
+			if data:
+				dest_host = addr[0]
+				dest_port = addr[1]
+				filename = data
+				# send error message if file does not exist
+				if filename not in files:
+					sock.send("File not found.", (dest_host, dest_port))
+				# if file exists, call post to send to client
+				else:
+					post(filename, sock, dest_host, dest_port)				
+			else:
+				continue
+	except:
+		print "Error"
+		raise # for debugging
  
 
 if __name__ == "__main__":
