41b188195be5a50830414fdc12182165a3f6a249
James Leopold <jleopold28@gmail.com>
2016-04-15 16:58:02 -0400

    fdsa

--- james_test/client/dbclientRTP.py
+++ james_test/client/dbclientRTP.py
@@ -5,60 +5,65 @@ import random
 sys.path.insert(0,'..')
 from rtp import *
 
-#split the first arument by colon
-hostPortList = sys.argv[1].split(":")
-#extract the server_host
-server_host = hostPortList[0]
-#extract the server_port
-server_port = int(hostPortList[1])
-#extract the query (ID)
-query = sys.argv[2]
-cols = "" #string to hold the cols requested
+sock = None
 
-for i in range(3, len(sys.argv)): #iterate over the remaining arguments
-	#seperate col values with commas, unless it is the last argument
-	if i == len(sys.argv) - 1:
-		cols = cols + sys.argv[i]
-	else:
-		cols = cols + sys.argv[i] +','
+def main(argv):
+	#split the first arument by colon
+	hostPortList = sys.argv[1].split(":")
+	#extract the server_host
+	server_host = hostPortList[0]
+	#extract the server_port
+	server_port = int(hostPortList[1])
+	#extract the query (ID)
+	query = sys.argv[2]
+	cols = "" #string to hold the cols requested
 
-try:
-	#INITIALIZATION
-	s = RTPSocket()
-	s.connect((server_host, server_port))
-	print s
-	time.sleep(2)
-	
-	#DATA TRANSFER
-	s.send(query + ":" + cols, (server_host, server_port))
+	for i in range(3, len(sys.argv)): #iterate over the remaining arguments
+		#seperate col values with commas, unless it is the last argument
+		if i == len(sys.argv) - 1:
+			cols = cols + sys.argv[i]
+		else:
+			cols = cols + sys.argv[i] +','
 
-	print "Sending Message to Server"
-	tries = 3
-	while tries > 0: #try to recieve response 3 times
-		try:
-			print "Waiting for a response..."
-			#receive the data from the server
-			data, addr = s.recv()
-			if data:
-				print "Received response from server"
-				# if we get a response, stop the loop
-				break
-			time.sleep(2)
-		except:
-			# if there is a timeout or the server is offline
-			print "The server has not answered in the last two seconds"
-			print "retrying..."
-			if tries == 1:
-				#if we are on the last try and there was no response, we set the error message
-				data = "There was no response from the server"
-		tries = tries - 1
+	try:
+		#INITIALIZATION
+		global sock
+		sock = RTPSocket()
+		sock.rwnd = 512
 
-	#TERMINATION
-	#close the socket
-	s.clientClose()
-except Exception, e:
-	#if there is an error, print error message
-	print e
-	data = "Error - Server may be offline"
+		conn = sock.connect((server_host, server_port))
+		
+		#DATA TRANSFER
+		sock.send(query + ":" + cols, (server_host, server_port))
 
-print data
+		print "Sending Message to Server"
+		tries = 3
+		while tries > 0: #try to recieve response 3 times
+			try:
+				print "Waiting for a response..."
+				#receive the data from the server
+				data = conn.getData()
+				if data:
+					print "Received response from server"
+					# if we get a response, stop the loop
+					break
+				time.sleep(2)
+			except:
+				# if there is a timeout or the server is offline
+				print "The server has not answered in the last two seconds"
+				print "retrying..."
+				if tries == 1:
+					#if we are on the last try and there was no response, we set the error message
+					data = "There was no response from the server"
+			tries = tries - 1
+
+		#TERMINATION
+		#close the socket
+		sock.clientClose()
+	except Exception, e:
+		#if there is an error, print error message
+		print e
+		data = "Error - Server may be offline"
+
+if __name__ == "__main__":
+    main(sys.argv)
--- james_test/server/dbengineRTP.py
+++ james_test/server/dbengineRTP.py
@@ -4,87 +4,93 @@ import csv
 sys.path.insert(0,'..')
 from rtp import *
 
-#server_host = '0.0.0.0'
-server_host = '127.0.0.1'
-#extract server_port argument
-server_port = int(sys.argv[1]);
-#initialize our database
-db = {}
-#format of db: {'ID' : ['first_name', 'last_name', 'quality_points', 'gpa_hours', 'gpa']}
+sock = None
 
-f = open("db.csv")
-#open the csv file holding the data
-reader = csv.reader(f);
-headers = reader.next();
-#read the csv file into db
-for row in reader:
-	db[str(row[0])] = [row[1], row[2], row[3], row[4], row[5]]
-#close the db.csv file
-f.close()
-
-try:
-	#INITIALIZATION
-	s = RTPSocket()
-	s.bind((server_host, server_port))
-	s.accept()
-	print s
-
-	#DATA TRANSFER
-	#receive the query and cols
-	#send the data back to the client
+def clientSession(conn,addr):
+	print "STARTING CLIENT SESSION at " + str(addr)
 	while 1:
-		#recieve the data from the client
-		data,addr = s.recv()
-		if data == "CLOSE CONNECTION":
-			break
-		elif data:
-			dest_host = addr[0]
-			dest_port = addr[1]
-		else:
-			continue
+		data = conn.getData()
+		if data:
+			if data == "CLOSE CONNECTION":
+				sock.serverClose(conn)
+				print "CLOSEING SESSION FOR " + str(addr) + "\n"
+				break
+			elif data:
+				dataList = data.split(":") #split the query and cols by colon
+				query = dataList[0].strip()
 
-		dataList = data.split(":") #split the query and cols by colon
-		query = dataList[0].strip()
+				if query not in db.keys():
+					# if the client requests an ID that we do not have, we send the error message
+					sock.send("Error: ID " + query + " not in the database!", addr)
+					continue
 
-		if query not in db.keys():
-			# if the client requests an ID that we do not have, we send the error message
-			s.send("Error: ID " + query + " not in the database!", (dest_host, dest_port))
-			continue
+				cols = dataList[1].split(",") #cols are comma delimited
+				#initialize our response
+				response = "From server: "
+				for col in cols:
+					col = col.strip()
+					if col == "first_name":
+						response = response + col + ": " + db[query][0]
+					elif col == "last_name":
+						response = response + col + ": " + db[query][1]
+					elif col == "quality_points":
+						response = response + col + ": " + db[query][2]
+					elif col == "gpa_hours":
+						response = response + col + ": " + db[query][3]
+					elif col == "gpa":
+						response = response + col + ": " + db[query][4]
+					else:
+						sock.send("Error: Client referring to a non-existing attribute - " + col,addr)
+						continue
 
-		cols = dataList[1].split(",") #cols are comma delimited
-		#initialize our response
-		response = "From server: "
-		for col in cols:
-			col = col.strip()
-			if col == "first_name":
-				response = response + col + ": " + db[query][0]
-			elif col == "last_name":
-				response = response + col + ": " + db[query][1]
-			elif col == "quality_points":
-				response = response + col + ": " + db[query][2]
-			elif col == "gpa_hours":
-				response = response + col + ": " + db[query][3]
-			elif col == "gpa":
-				response = response + col + ": " + db[query][4]
-			else:
-				# if the client asks for a col that does not exist, we send the error message
-				response = "Error: Client referring to a non-existing attribute - " + col
-				#s.send("Error: Client referring to a non-existing attribute - " + col, (dest_host, dest_port))
-				#continue
-			if col != cols[len(cols) - 1].strip():
-				response = response + ", " #do not add a comma for the last element
+					if col != cols[len(cols) - 1].strip():
+							response = response + ", " #do not add a comma for the last element
 
-		if response[:5] == "Error":
-			s.send(response, (dest_host, dest_port))
-			continue
-		else:
-			s.send(response, (dest_host, dest_port))
-			continue
+					if response[:5] == "Error":
+						s.send(response, addR)
+						continue
+					else:
+						s.send(response, addr)
+						continue
 
-	#TERMINATION
-	#This is unreachable becuase crtl-c does not terminate the while loop, instead you must close the window
-	s.serverClose()
-except Exception, e:
-	#if we find an exception above, we print error message
-	print "Error accessing the server, please try again"
-	print e
+	return
+
+def main(argv):
+	server_host = '127.0.0.1'
+	server_port = int(argv[1])
+	#initialize our database
+	db = {} #format of db: {'ID' : ['first_name', 'last_name', 'quality_points', 'gpa_hours', 'gpa']}
+
+	f = open("db.csv")
+	#open the csv file holding the data
+	reader = csv.reader(f);
+	headers = reader.next();
+	#read the csv file into db
+	for row in reader:
+		db[str(row[0])] = [row[1], row[2], row[3], row[4], row[5]]
+	#close the db.csv file
+	f.close()
+
+	try:
+		#INITIALIZATION
+
+		sock = RTPSocket() #create only 1 socket
+		sock.rwnd = 512
+		sock.bind((server_host, server_port))
+		while 1:
+			#check the SYN QUE in ACCEPT
+			conn, addr = sock.accept()
+			if (conn, addr) != ( "","" ):
+				newthread = threading.Thread(target = clientSession, args = (conn, addr,))
+				newthread.start()
+
+		#TERMINATION
+		#This is unreachable becuase crtl-c does not terminate the while loop, instead you must close the window
+		s.serverClose()
+	except Exception, e:
+		#if we find an exception above, we print error message
+		print "Error accessing the server, please try again"
+		print e
+
+if __name__ == "__main__":
+    main(sys.argv)
